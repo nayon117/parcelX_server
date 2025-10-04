c2241091bc0af9f3b3a96bcf40c77216202a7502
nayon117 <hasibul.nayon1@gmail.com>
2025-10-04 10:33:37 +0600

    create rider apis

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
 const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_KEY);
+const admin = require("firebase-admin");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+const serviceAccount = require("./firebase-admin-key.json");
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oesmq38.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,24 +27,135 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-    const db = client.db('parcelXDB');
-    const parcelCollection = db.collection('parcels');
-    const paymentCollection = db.collection('payments');
+    const db = client.db("parcelXDB");
+    const parcelCollection = db.collection("parcels");
+    const paymentCollection = db.collection("payments");
+    const trackingCollection = db.collection("trackings");
+    const userCollection = db.collection("users");
+    const riderCollection = db.collection("riders");
 
+    // custom middlewares
+
+    // verify Token
+    const verifyToken = async (req, res, next) => {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+      const token = authHeader.split(" ")[1];
+
+      if (!token) {
+        return res.status(401).send({ message: "Unauthorized" });
+      }
+
+      try {
+        const decodedUser = await admin.auth().verifyIdToken(token);
+        req.decodedUser = decodedUser;
+        next();
+      } catch (error) {
+        console.error("Error verifying token:", error);
+        res.status(403).send({ message: "Forbidden" });
+      }
+    };
+
+    // riders collection---------------------
+    //POST: add a new rider
+    app.post("/riders", async (req, res) => {
+      try {
+        const newRider = req.body;
+        const result = await riderCollection.insertOne(newRider);
+        res.send(result);
+      } catch (error) {
+        console.error("Error adding rider:", error);
+        res.status(500).send("Error adding rider");
+      }
+    });
+
+    // GET: all pending riders
+    app.get("/riders/pending", async (req, res) => {
+      try {
+        const pendingRiders = await riderCollection
+          .find({ status: "pending" })
+          .toArray();
+        res.send(pendingRiders);
+      } catch (error) {
+        console.error("Error fetching pending riders:", error);
+        res.status(500).send("Error fetching pending riders");
+      }
+    });
+
+    // GET: all approved riders
+    app.get("/riders/approved", async (req, res) => {
+      try {
+        const approvedRiders = await riderCollection
+          .find({ status: "approved" })
+          .toArray();
+        res.send(approvedRiders);
+      } catch (error) {
+        console.error("Error fetching approved riders:", error);
+        res.status(500).send("Error fetching approved riders");
+      }
+    });
+
+    // PATCH: update rider status by id
+
+    app.patch("/riders/:id/status", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const { status } = req.body; 
+
+        const result = await riderCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { status } }
+        );
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ error: "Something went wrong" });
+      }
+    });
+
+    // users collection---------------------
+    // POST: add a new user if not exists
+    app.post("/users", async (req, res) => {
+      try {
+        const newUser = req.body;
+        const query = { email: newUser.email };
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          // update last_login time
+          const updateDoc = {
+            $set: { last_login: new Date().toISOString() },
+          };
+          await userCollection.updateOne(query, updateDoc);
+          return res
+            .status(200)
+            .send({ message: "User already exists", inserted: false });
+        }
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+      } catch (error) {
+        console.error("Error adding user:", error);
+        res.status(500).send("Error adding user");
+      }
+    });
+
+    // parcel collection routes---------------------
     // GET all parcels or by user email and sorted by latest
-    app.get('/parcels', async (req, res) => {
+    app.get("/parcels", verifyToken, async (req, res) => {
       try {
         const email = req.query.email;
         const query = email ? { created_by: email } : {};
         const options = {
-          sort: { createdAt: -1 }
+          sort: { createdAt: -1 },
         };
 
         const parcels = await parcelCollection.find(query, options).toArray();
@@ -51,7 +167,7 @@ async function run() {
     });
 
     // get a single parcel by id
-    app.get('/parcels/:id', async (req, res) => {
+    app.get("/parcels/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
@@ -64,7 +180,7 @@ async function run() {
     });
 
     // add a new parcel
-    app.post('/parcels', async (req, res) => {
+    app.post("/parcels", async (req, res) => {
       try {
         const newParcel = req.body;
         const result = await parcelCollection.insertOne(newParcel);
@@ -76,7 +192,7 @@ async function run() {
     });
 
     // delete a parcel by _id
-    app.delete('/parcels/:id', async (req, res) => {
+    app.delete("/parcels/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
@@ -88,13 +204,40 @@ async function run() {
       }
     });
 
+    // Trackings
+    app.post("/tracking", async (req, res) => {
+      const {
+        tracking_id,
+        parcel_id,
+        status,
+        message,
+        updated_by = "",
+      } = req.body;
+
+      const log = {
+        tracking_id,
+        parcel_id: parcel_id ? new ObjectId(parcel_id) : undefined,
+        status,
+        message,
+        time: new Date(),
+        updated_by,
+      };
+
+      const result = await trackingCollection.insertOne(log);
+      res.send({ success: true, insertedId: result.insertedId });
+    });
+
+    // payment collection routes-------------------
     // GET: get payment by email or all payments also sorted by latest
-    app.get('/payments', async (req, res) => {
+    app.get("/payments", verifyToken, async (req, res) => {
       try {
         const email = req.query.email;
+        if (req.decodedUser.email !== email) {
+          return res.status(403).send({ message: "Forbidden" });
+        }
         const query = email ? { email } : {};
         const options = {
-          sort: { paid_at: -1 }
+          sort: { paid_at: -1 },
         };
         const payments = await paymentCollection.find(query, options).toArray();
         res.send(payments);
@@ -105,16 +248,16 @@ async function run() {
     });
 
     // POST: record payment and update parcel status
-    app.post('/payments', async (req, res) => {
+    app.post("/payments", async (req, res) => {
       try {
-        const {id, email, amount, paymentMethod, transactionId } = req.body;
+        const { id, email, amount, paymentMethod, transactionId } = req.body;
 
         const updateResult = await parcelCollection.updateOne(
           { _id: new ObjectId(id) },
-          { $set: { payment_status: 'paid'} }
+          { $set: { payment_status: "paid" } }
         );
 
-        if(updateResult.modifiedCount === 0) {
+        if (updateResult.modifiedCount === 0) {
           return res.status(404).send("Parcel not found or already paid");
         }
 
@@ -125,35 +268,31 @@ async function run() {
           paymentMethod,
           transactionId,
           paid_at_string: new Date().toISOString(),
-          paid_at: new Date()
-        }
+          paid_at: new Date(),
+        };
         const paymentResult = await paymentCollection.insertOne(paymentDoc);
 
         res.status(201).send({
           message: "Payment recorded and parcel updated",
           updateResult,
-          insertedId: paymentResult.insertedId
-         });
-       
+          insertedId: paymentResult.insertedId,
+        });
       } catch (error) {
         console.error("Error processing payment:", error);
         res.status(500).send("Error processing payment");
       }
     });
 
-
-
     // create payment intent
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post("/create-payment-intent", async (req, res) => {
       try {
         const { amount } = req.body;
 
         // Create a payment intent using Stripe API
         const paymentIntent = await stripe.paymentIntents.create({
           amount,
-          currency: 'usd', 
-          payment_method_types: ['card'],
-
+          currency: "usd",
+          payment_method_types: ["card"],
         });
 
         res.json({ clientSecret: paymentIntent.client_secret });
@@ -165,7 +304,9 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -173,15 +314,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 // Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to ParcelX API');
+app.get("/", (req, res) => {
+  res.send("Welcome to ParcelX API");
 });
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
